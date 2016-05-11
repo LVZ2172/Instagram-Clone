0c379cdebe6cbf5e10bcea8379031b37911d96b5
Leon Zagorouiko <Zagorouiko@gmail.com>
2016-05-10 18:08:11 -0700

    add initial files, connect to parse server, set up login feature, and add a second activity to show user list.

---
+++ ParseStarterProject/src/main/java/com/parse/starter/UserList.java
@@ -0,0 +1,64 @@
+package com.parse.starter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserList extends AppCompatActivity {
+
+    ArrayList<String> usernames;
+    ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_list);
+        usernames = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, usernames);
+
+
+        final ListView userList = (ListView) findViewById(R.id.userList);
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+
+        query.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+        query.addAscendingOrder("username");
+
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+
+                if (e == null) {
+
+                    if (objects.size() > 0) {
+
+
+
+                        for (ParseUser user : objects) {
+
+                            usernames.add(user.getUsername());
+
+                        }
+
+
+                        userList.setAdapter(arrayAdapter);
+
+                    }
+
+
+
+                }
+
+            }
+        });
+    }
+}
