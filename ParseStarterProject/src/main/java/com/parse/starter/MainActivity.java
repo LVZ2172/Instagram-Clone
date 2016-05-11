09dcaaca5c436d6127243f97c9e1a33a0fa0f3d0
Leon Zagorouiko <Zagorouiko@gmail.com>
2016-05-10 22:35:51 -0700

    add user feed and sharing feature.

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -165,7 +165,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     int id = item.getItemId();
 
     //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
+    if (id == R.id.share) {
       return true;
     }
 
--- ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
+++ ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -37,7 +37,7 @@ public class StarterApplication extends Application {
     );
 
 
-      ParseUser.enableAutomaticUser();
+//      ParseUser.enableAutomaticUser();
     ParseACL defaultACL = new ParseACL();
     // Optionally enable public read access.
     // defaultACL.setPublicReadAccess(true);
